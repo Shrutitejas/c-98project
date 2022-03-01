b1f59266fc7d5ea83b9e7bd7738ee5751c23af61
Shrutitejas <96972363+Shrutitejas@users.noreply.github.com>
2022-03-01 22:15:54 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+var speechRecognition = window.webkitSpeechRecognition;
+var recognition = new speechRecognition();
+
+function start(){
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+
+recognition.onresult = function(event){
+    console.log(event);
+    content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = content;
+    if (content == "take my selfie"){
+        speak();
+    }
+}
+function speak(){
+    synth = window.speechSynthesis;
+    speech_data = "Taking your selfie in 5 seconds";
+    utterThis = new SpeechSynthesisUtterance(speech_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+    setTimeout(function(){
+      take_snapshot();
+      save();
+    },5000);
+}
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width :360,
+    height :250,
+    image_format :"png",
+    jpeg_quality :90
+});
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+       document.getElementById("result").innerHTML='<img id="selfie_img" src="'+data_uri+'"/>'
+    });
+}
+
+function save(){
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_img").src;
+    link.href = image;
+    link.click();
+}
